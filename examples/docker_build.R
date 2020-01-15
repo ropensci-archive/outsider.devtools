b190649e72ebfb5e82cef45abe75e36a8afc400f
Dominic Bennett <dominic.john.bennett@gmail.com>
2020-01-15 16:19:58 +0100

    cannot run examples that depend on docker, CI systems might not have it

--- examples/docker_build.R
+++ examples/docker_build.R
@@ -1,19 +1,21 @@
 library(outsider.devtools)
 
-# simplest possible Dockerfile
-df_text <- "
-FROM ubuntu:latest
-"
-
-# create dir to host Dockerfile
-flpth <- file.path(tempdir(), 'test_docker_build')
-if(!dir.exists(flpth)) {
-  dir.create(flpth)
-}
-
-# write to file
-write(x = df_text, file = file.path(flpth, 'Dockerfile'))
-
-# run docker_build from flpth
-docker_build(img = 'test_docker_build', tag = 'latest', verbose = TRUE,
-             url_or_path = flpth)
+# # NOT RUN
+# 
+# # simplest possible Dockerfile
+# df_text <- "
+# FROM ubuntu:latest
+# "
+# 
+# # create dir to host Dockerfile
+# flpth <- file.path(tempdir(), 'test_docker_build')
+# if(!dir.exists(flpth)) {
+#   dir.create(flpth)
+# }
+# 
+# # write to file
+# write(x = df_text, file = file.path(flpth, 'Dockerfile'))
+# 
+# # run docker_build from flpth
+# docker_build(img = 'test_docker_build', tag = 'latest', verbose = TRUE,
+#              url_or_path = flpth)
