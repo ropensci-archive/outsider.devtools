d4f52782316c3fec332d978c4bc8517173bd793d
Dominic Bennett <dominic.john.bennett@gmail.com>
2020-01-15 15:07:04 +0100

    reexporting base functions

---
+++ R/import_from_base.R
@@ -0,0 +1,89 @@
+# import outsider.base functions
+
+#' @importFrom outsider.base arglist_get
+#' @export
+outsider.base::arglist_get
+
+#' @importFrom outsider.base arglist_parse
+#' @export
+outsider.base::arglist_parse
+
+#' @importFrom outsider.base dirpath_get
+#' @export
+outsider.base::dirpath_get
+
+#' @importFrom outsider.base filestosend_get
+#' @export
+outsider.base::filestosend_get
+
+#' @importFrom outsider.base wd_get
+#' @export
+outsider.base::wd_get
+
+#' @importFrom outsider.base docker_img_ls
+#' @export
+outsider.base::docker_img_ls
+
+#' @importFrom outsider.base image_install
+#' @export
+outsider.base::image_install
+
+#' @importFrom outsider.base is_docker_available
+#' @export
+outsider.base::is_docker_available
+
+#' @importFrom outsider.base outsider_init
+#' @export
+outsider.base::outsider_init
+
+#' @importFrom outsider.base run
+#' @export
+outsider.base::run
+
+#' @importFrom outsider.base is_installed
+#' @export
+outsider.base::is_installed
+
+#' @importFrom outsider.base meta_get
+#' @export
+outsider.base::meta_get
+
+#' @importFrom outsider.base modules_list
+#' @export
+outsider.base::modules_list
+
+#' @importFrom outsider.base pkg_install
+#' @export
+outsider.base::pkg_install
+
+#' @importFrom outsider.base uninstall
+#' @export
+outsider.base::uninstall
+
+#' @importFrom outsider.base cat_line
+#' @export
+outsider.base::cat_line
+
+#' @importFrom outsider.base char
+#' @export
+outsider.base::char
+
+#' @importFrom outsider.base func
+#' @export
+outsider.base::func
+
+#' @importFrom outsider.base log_set
+#' @export
+outsider.base::log_set
+
+#' @importFrom outsider.base stat
+#' @export
+outsider.base::stat
+
+#' @importFrom outsider.base server_connect
+#' @export
+outsider.base::server_connect
+
+#' @importFrom outsider.base server_disconnect
+#' @export
+outsider.base::server_disconnect
